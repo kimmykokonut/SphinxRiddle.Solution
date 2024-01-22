c0ee781ef5d9147b979be5bb59ba780a02a63538
kimmykokonut@gmail <kimmykokonut@gmail.com>
2024-01-22 14:33:10 -0800

    initial project commit
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

---
+++ SphinxRiddle.Tests/ModelTests/RiddleVariablesTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SphinxRiddle.Models;
+
+namespace SphinxRiddle.Tests
+{
+  [TestClass]
+  public class RiddleVariablesTests
+  {
+    // Test methods go here
+  }
+}
